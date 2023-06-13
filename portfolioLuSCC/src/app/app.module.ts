8a2a047cd1a8f5cf4b1972dd8cd84894ef36cea6
lucianatux <lucianatux@yahoo.com.ar>
2023-06-13 14:54:12 -0300

    header

--- portfolioLuSCC/src/app/app.module.ts
+++ portfolioLuSCC/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    ProyectosComponent,
+    SkillsComponent,
   ],
   imports: [
     BrowserModule,
